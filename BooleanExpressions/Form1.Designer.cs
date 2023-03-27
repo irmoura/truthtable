180ed5a1affe1de5f9bb647340b11058240a77ec
Ismael Ribeiro <coragethedogcowardle@gmail.com>
2023-03-27 14:15:26 -0300

    Code upload

---
+++ BooleanExpressions/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace BooleanExpressions
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 371);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dataGridView1_CellFormatting);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(776, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 4000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Truth table";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ BooleanExpressions/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BooleanExpressions
+{
+    public partial class Form1 : Form
+    {
+        List<string> letras = new List<string>() { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r" };
+        int count = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int distance = 45;
+            textBox1.Size = new Size(this.Width - distance, 20);
+            btnStart.Size = new Size(this.Width - distance, 23);
+            //776; 371
+            dataGridView1.Size = new Size(this.Width - distance, this.Height - 125);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            //if (this.dataGridView1.Columns[e.ColumnIndex].Name == "a") // Substitua "ColumnName" pelo nome da coluna que deseja verificar
+            //{
+            if (Convert.ToInt32(e.Value) > 0) // Se o valor da célula for maior que zero
+            {
+                e.CellStyle.BackColor = Color.Green; // Definir cor da célula como verde
+            }
+            else
+            {
+                e.CellStyle.BackColor = Color.Red; // Definir cor da célula como vermelha
+            }
+            //}
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (count <= letras.Count)
+            {
+                textBox1.Clear();
+                for (int i = 0; i < count; i++)
+                {
+                    textBox1.Text += $"{letras[i]}";
+                }
+                count++;
+                timer1.Enabled = false;
+                backgroundWorker1.RunWorkerAsync();
+            }
+            else
+            {
+                MessageBox.Show($"{dataGridView1.Rows.Count} possibilities");
+                timer1.Enabled = false;
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //dataGridView1.DataSource = null;
+            dataGridView1.Invoke((MethodInvoker)delegate
+            {
+                dataGridView1.DataSource = null;
+            });
+            char[] preposicoes = textBox1.Text.ToArray();
+            double columnCount = preposicoes.Length;
+            double lineCount = Math.Pow(2, columnCount);
+            double division = lineCount / 2;
+            double count_ = 0;
+            DataTable dt = new DataTable();
+            int[,] _lines = new int[(int)columnCount, (int)lineCount];//colunas - linhas
+            List<string> lines = new List<string>();
+            for (int i = 0; i < columnCount; i++)//colunas
+            {
+                dt.Columns.Add($"{preposicoes[i]}");
+                for (int j = 0; j < (int)lineCount; j++)//linhas
+                {
+                    int value;
+                    if (count_ < division)
+                    {
+                        value = 0;
+                        count_++;
+                    }
+                    else
+                    {
+                        value = 1;
+                        count_++;
+                        if (count_ == division * 2)
+                        {
+                            count_ = 0;
+                        }
+                    }
+                    _lines[i, j] = value;
+                    //Console.WriteLine($"{value}");
+                }
+                count_ = 0;
+                division /= 2;
+            }
+            // Percorrer o array e adicionar as linhas ao DataTable
+            for (int i = 0; i < _lines.GetLength(1); i++)//linhas
+            {
+                for (int j = 0; j < _lines.GetLength(0); j++)//colunas
+                {
+                    lines.Add($"{_lines[j, i]}");
+                }
+                dt.Rows.Add(lines.ToArray());
+                lines.Clear();
+            }
+            //dataGridView1.DataSource = dt;
+            dataGridView1.Invoke((MethodInvoker)delegate
+            {
+                dataGridView1.DataSource = dt;
+            });
+            //
+            dataGridView1.Rows[0].DefaultCellStyle.SelectionBackColor = Color.Red;
+            dataGridView1.Rows[0].DefaultCellStyle.SelectionForeColor = Color.Black;
+            //
+            // Centralizar o texto das células e cabeçalhos do DataGridView
+            dataGridView1.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridView1.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            //MessageBox.Show($"{preposicoes.Length} propositions | {dataGridView1.Rows.Count} possibilities");
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+    }
+}
